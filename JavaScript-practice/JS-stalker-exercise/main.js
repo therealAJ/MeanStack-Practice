8570567ff48a3125886abd215bc67450cb6ead86
Alex <alexonej@gmail.com>
2015-12-30 15:08:52 -0800

    Finished excercise1

---
+++ JavaScript-practice/JS-stalker-exercise/main.js
@@ -0,0 +1,6 @@
+var firstName = prompt("Whats your first name?");
+var lastName = prompt("Whats your last name?");
+var age = prompt("How old are you?");
+
+alert("Your name is " + firstName+ " " + lastName);
+alert("And you are " + age + " years old");
