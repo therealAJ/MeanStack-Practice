f5e1fd73ce8b1916536f563ac3ec879bd176b71e
Alex <alexonej@gmail.com>
2015-12-30 22:14:46 -0800

    added more function practice

---
+++ JavaScript-practice/Functions-Practice/main.js
@@ -0,0 +1,25 @@
+function isEven(int) {
+    
+    if(int % 2 == 0)
+        return true; 
+    else return false; 
+}
+
+
+
+function factorial(num) {
+    
+    var result = 1; 
+    
+    for(var i = 1; i < num ; i++) {
+        
+        result = result * i; 
+    }
+    
+    return result; 
+    
+   
+}
+
+
+// factorial (4) = 4 X 3 X 2 X 1
