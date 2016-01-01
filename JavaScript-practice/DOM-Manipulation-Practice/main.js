9343e32ab64159ca88087a21391f9c502b512bfa
Alex <alexonej@gmail.com>
2016-01-01 15:05:03 -0800

    practiced event listeners

---
+++ JavaScript-practice/DOM-Manipulation-Practice/main.js
@@ -0,0 +1,14 @@
+//var p1 = document.getElementById("first");
+//var p1 = document.getElementsByTagName(p1)[0];
+//var p1 = document.querySelector("#first");
+//var p1 = document.querySelector(".special");'
+
+var btn = document.querySelector("button");
+var body = document.querySelector("body");
+body.classList.add("purple-background");
+
+btn.addEventListener("click", function() {
+    
+    body.classList.toggle("purple-background");
+    
+})
