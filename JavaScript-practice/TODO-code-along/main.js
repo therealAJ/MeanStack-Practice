648b4ba5b5e4c0c96c12596ff0616655302d6217
Alex <alexonej@gmail.com>
2015-12-31 22:14:21 -0800

    enhanced todo-list

--- JavaScript-practice/TODO-code-along/main.js
+++ JavaScript-practice/TODO-code-along/main.js
@@ -2,16 +2,51 @@ var userInput = prompt("What would you like to do?");
 var todos = [];
 
 
-while(userInput !== "quit") {
+while (userInput !== "quit") {
 
-if (userInput ==="new") {
-    var newTodo = prompt("Enter a new todo");
-    todos.push(newTodo);
-    
-} else if (userInput === "list") {
-    console.log(todos);
-   
-} 
- userInput = prompt("What would you like to do?");
+    if (userInput === "new") {
+        
+        addTodo(); 
+
+    } else if (userInput === "list") {
+        
+        listTodo(); 
+
+    } else if (userInput === "delete") {
+       
+        deleteTodo(); 
+    }
+
+
+    userInput = prompt("What would you like to do?");
 }
 
+function addTodo() {
+
+    var newTodo = prompt("Enter a new todo");
+    todos.push(newTodo);
+    console.log(newTodo + " added to list");
+
+}
+
+function listTodo() {
+    
+    console.log("********")
+    todos.forEach(function (todoElement, index) {
+
+        console.log(index + ":" +
+            todoElement);
+
+    });
+    console.log("********")
+
+}
+
+function deleteTodo() {
+    
+   var indexToDelete = prompt("Enter index of todo to delete");
+        todos.splice(indexToDelete, 1);
+        console.log("Item at index " + indexToDelete + " was deleted");
+    
+    
+}
