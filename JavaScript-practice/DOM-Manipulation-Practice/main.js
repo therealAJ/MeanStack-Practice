0d5077889702218181d536aac2cef0b8627b55d0
Alex <alexonej@gmail.com>
2016-01-02 00:56:27 -0800

    updated workspace

--- JavaScript-practice/DOM-Manipulation-Practice/main.js
+++ JavaScript-practice/DOM-Manipulation-Practice/main.js
@@ -1,7 +1,7 @@
 //var p1 = document.getElementById("first");
 //var p1 = document.getElementsByTagName(p1)[0];
 //var p1 = document.querySelector("#first");
-//var p1 = document.querySelector(".special");'
+//var p1 = document.querySelector(".special");'  // 4 different ways to select elements
 
 var btn = document.querySelector("button");
 var body = document.querySelector("body");
