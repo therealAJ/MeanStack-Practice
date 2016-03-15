f99318f8d1ff38909c03dfaac45a82de2e78b504
Alex <alexonej@gmail.com>
2016-03-15 00:52:19 -0700

    added middleware + token verification

--- User-Story/app/routes/api.js
+++ User-Story/app/routes/api.js
@@ -99,6 +99,46 @@ module.exports = function(app, express) {
         });
     });
     
-    return api; 
+    // MIDDLEWARE
+    
+    api.use(function (req, res, next) {
+     
+        console.log("Somebody just came to our app"); 
+        
+        var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+        
+        // check if token exists 
+        if (token) {
+            
+            jsonwebtoken.verify(token, secretKey, function(err, decoded) {
+                
+                if(err) {
+                    res.status(403).send({success: false, message: "Failed to authenticate user"});
+                } else {
+                    
+                    req.decoded = decoded; 
+                    
+                    next(); 
+                
+                }
+            });
+        } else {
+
+            res.status(403).send({ success: false, message: "No Token Provided"});
+        }
+    });
+    
+    
+    //DESTINATION B // Provide a legitmate token
+    
+    api.get('/', function(req, res) {
+        
+        res.json("Hello World!");
+        
+    });
+    
+    
+
+    return api
     
 }
